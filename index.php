a83658579e942e007bfa5420d92fa795380d5167
Dave Kellam <dave@eightface.com>
2015-03-25 23:24:18 -0400

    Adding title/content to theme

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <html>
 <head>
 
+<title><?php the_title(); ?></title>
+
 <?php wp_head(); ?>
 
 </head>
 <body>
+
+<h1><?php the_title(); ?></h1>
+
+<?php the_content(); ?>
+
 
 <?php wp_footer(); ?>
 
